a00a8202d3429038fb1500d3fc4826a58e36ea77
Nikolai <npesudovs@outlook.com>
2022-08-18 14:05:00 +0930

    Imported Grammar and random

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-#Pygame Tutorial 1
-#main.py
-#Nikolai Pesudovs
-#5th August 2022
-#A basic pygametutorial
+# Pygame Tutorial 1
+# main.py
+# Nikolai Pesudovs
+# 5th August 2022
+# A basic pygametutorial
 
-#Imports Pygame
+# Imports 
 from ast import While
 import pygame
+import random
 
-#Defining Colours
+
+# Defining Colours
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
@@ -16,26 +18,29 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 TEAL = (0, 255, 255)
 
-#preps image
+# preps image
 BACKGROUND = pygame.image.load("images/backround.png")
 BACKGROUND = pygame.transform.scale(BACKGROUND, [1280, 720], )
 
-#Window Settings
+# Window Settings
 size = (1280, 720)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Betwix Yall")
 
-#preps the game
+# preps the game
 pygame.init()
 
-#Loop to keep game running
-if __name__ == "__main__":     
+# Loop to keep game running
+if __name__ == "__main__":
     while True:
-        for event in pygame.event.get(): #Event Loop
-            if event.type == pygame.QUIT: #Lets game be closed
+        for event in pygame.event.get():  # Event Loop
+            if event.type == pygame.QUIT:  # Lets game be closed
                 pygame.quit()
-        
+
+        # Game Logic
+
         ## Drawing ##
         screen.blit(BACKGROUND, [0, 0, 1280, 720])
+
         ##
         pygame.display.update()
